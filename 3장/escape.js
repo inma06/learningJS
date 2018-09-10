8f95c5494e11fcc09dad312483e576862bf4e582
mkw8263 <ruddns8213@naver.com>
2018-09-11 01:36:15 +0900

    첫번째 3장 escape,templet

---
+++ 3장/escape.js
@@ -0,0 +1,12 @@
+const dialog= 'Sam looked up, and said "hello, old freiend!", as Max walked in';
+
+const imperative = "Don't do that!"
+
+// 위 내용 정상 이상 무!
+
+const dialog = "Sam looked up and said \"don't do that!\" to Max.";
+// 역슬래시를 써서 문자열이 끝나지 않음을 자스에게 알릴수있음!! 굿!!
+
+const s="In JavaScript, use \\as an escape character in strings";
+// 역슬래시를 쓰고 싶을때는 요렇게~~
+// 큰따움표를 쓰던 작은 따옴표를 쓰던.. 개발자 맘..
---
+++ 3장/templet.js
@@ -0,0 +1,5 @@
+let currntTemp = 19.5;
+
+const message= `the currnet temperature is ${currntTemp}`;
+//작은 다옴표 대신 백틱? `` 이걸 쓰게되면 코틀린과 동일한 템플릿문자열을 사용 가능~ 
+console.log(message);
